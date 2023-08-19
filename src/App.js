1d08afeeb442de5fca279c00592a397d68735fb4
JCD <juliocesar97db@gmail.com>
2023-08-19 15:50:30 -0400

    changes for V2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from V2 of my first web page deployed on AWS
         </p>
         <a
           className="App-link"
