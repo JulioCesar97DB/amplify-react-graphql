2ae05b627d666fbf346d5b12f7a4f52464848abd
JCD <juliocesar97db@gmail.com>
2023-11-15 10:10:01 -0500

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mojona t quiero
+          asdf
         </p>
         <a
           className="App-link"
